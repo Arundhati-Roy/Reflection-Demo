4c8c93bf83562526a3743438bd0c038b91e8c9f4
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-13 10:31:26 +0530

    Reflection Demo

---
+++ Reflection/CustomerInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Reflection
+{
+    public class CustomerInfo
+    {
+        public CustomerInfo()
+        {
+            this.Id = -1;
+            this.FullName = string.Empty;
+        }
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public CustomerInfo(int id,string name)
+        { 
+            this.Id = id;
+            this.FullName = name;
+        }
+        public void PrintID()
+        {
+            Console.WriteLine("ID is:{0}", this.Id);
+        }
+        public void PrintName()
+        {
+            Console.WriteLine("Name is:{0}", this.FullName);
+        }
+    }
+}
---
+++ Reflection/ReflectionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace Reflection
+{
+    class ReflectionCust
+    {
+        public static void Test()
+        {
+            Type type = Type.GetType("Reflection.CustomerInfo");
+            Console.WriteLine("Full Name:{0}", type.FullName);
+            Console.WriteLine("Class Name:{0}", type.Name);
+            Console.WriteLine("Methods in Customer class");
+            MemberInfo[] methods = type.GetMethods();
+
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine(method.ReturnType.Name + " " + method.Name);
+            }
+
+            Console.WriteLine("Properties in Customer class");
+            PropertyInfo[] properties = type.GetProperties();
+            foreach(PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.PropertyType.Name);
+            }
+
+            Console.WriteLine("Constructors in Customer class");
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach (ConstructorInfo cons in constructors)
+            {
+                Console.WriteLine(cons.ToString());
+            }
+        }
+    }
+    
+}
